2a2834c4eaa694040134279df5d201bf46111ef4
Jens Eriksson <konsult.jens.eriksson@spv.se>
2021-10-07 00:01:43 +0200

    Update to BundleConfig

--- SecondHandMarket.Web/App_Start/BundleConfig.cs
+++ SecondHandMarket.Web/App_Start/BundleConfig.cs
@@ -16,7 +16,8 @@ namespace SecondHandMarket.Web
                         "~/js/jquery.cookie.js",
                         "~/js/bootstrap.min.js",
                         "~/js/bootstrap3-typeahead.js",
-                        "~/js/site.js"
+                        "~/js/site.js",
+                        "~/js/tinymce/tinymce.min.js"
                         ));
 
             bundles.Add(new StyleBundle("~/css").Include(
